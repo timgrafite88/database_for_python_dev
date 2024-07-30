d1ae9071151cf0ac6ca0a984abe28dfa3e4b8140
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-07-30 22:00:41 +0300

    update homework 30-07-2024

--- Database.py
+++ Database.py
@@ -15,30 +15,25 @@ def create_db(conn):
                 create table phones (phone_number int check(len(phone_number) = 10),
                    client_id int references clients(client_id));""")
 
-    conn.commit()
-
-
 def add_client(conn, first_name, last_name, email, phones=None):
     """Функция добавления клиента"""
     cur = conn.cursor()
 
-    cur.execute(f"""
+    cur.execute("""
     insert into clients (first_name, last_name, email) 
-    values ('{first_name}', '{last_name}', '{email}'); 
-                """)
-    conn.commit()
+    values (%s, %s, %s); 
+                """, (first_name, last_name, email))
 
     #добавляем телефон, если он указан
     if phones is not None:
-        cur.execute(f"""
+        cur.execute("""
         insert into phones (client_id, phone_number)
         values ((select client_id 
                 from clients 
-                where first_name = '{first_name}'
-                    and last_name = '{last_name}'
-                    and email = '{email}'), {phones});
-        """)
-    conn.commit()
+                where first_name = %s
+                    and last_name = %s
+                    and email = %s), %s);
+        """, (first_name, last_name, email, phones))
 
 
 def add_phone(conn, client_id, phone):
@@ -46,77 +41,96 @@ def add_phone(conn, client_id, phone):
     cur = conn.cursor()
     cur.execute(f"""
                 insert into phones (client_id, phone)
-                values ({client_id}, {phone});
-                """)
-    conn.commit()
+                values (%s, %s);
+                """, (client_id, phone))
 
 def change_client(conn, client_id, first_name=None, last_name=None, email=None, phones=None):
     """Функция изменения данных клиента"""
     cur = conn.cursor()
 
+    #если в функцию подаем номер телефона, то заменяем его
     if phones is not None:
-        cur.execute(f"""update phones set phone_number = {phones} where client_id = {client_id};""")
+        cur.execute("""
+        update phones
+        set phone_number = %s
+        where client_id = %s;
+        """, (phones, client_id)
+        )
 
     if first_name is not None:
-        cur.execute(f"""update clients set first_name = {first_name} where client_id = {client_id};""")
+        cur.execute("""
+        update clients
+        set first_name = %s
+        where client_id = %s;
+        """, (first_name, client_id)
+        )
 
     if last_name is not None:
-        cur.execute(f"""update clients set last_name = {last_name} where client_id = {client_id};""")
+        cur.execute("""
+        update clients
+        set last_name = %s
+        where client_id = %s;
+        """, (last_name, client_id)
+        )
 
     if email is not None:
-        cur.execute(f"""update clients set email = {email} where client_id = {client_id};""")
+        cur.execute("""
+            update clients
+            set email = %s
+            where client_id = %s;
+            """, (email, client_id)
+                )
 
-    conn.commit()
 
 
 def delete_phone(conn, client_id, phone):
     """Функция удаления телефона"""
     cur = conn.cursor()
-    cur.execute(f"""
+    cur.execute("""
                     delete from phones
-                    where client_id = {client_id}
-                        and phone = {phone};
-                    """)
-    conn.commit()
+                    where client_id = %s
+                        and phone = %s;
+                    """, (client_id, phone))
 
 
 def delete_client(conn, client_id):
     """Функция удаления информации о клиенте"""
     cur = conn.cursor()
-    cur.execute(f"""
+    cur.execute("""
                         delete from phones
-                        where client_id = {client_id};
+                        where client_id = %s;
                         
                         delete from clients
-                        where client_id = {client_id};
-                        """)
-    conn.commit()
+                        where client_id = %s;
+                        """, (client_id, ))
+
 
 def find_client(conn, first_name=None, last_name=None, email=None, phone=None):
     """Функция поиска клиента по данным"""
     cur = conn.cursor()
 
     if phone is not None:
-        cur.execute(f"""select first_name, last_name, email, phone_number
+        cur.execute("""select first_name, last_name, email, phone_number
                                 from clients c
                                     join phones p on c.client_id = p.client_id
-                                where p.phone_number = {phone}
-                                ;""")
+                                where p.phone_number = %s
+                                ;""", (phone,))
 
     else:
-        cur.execute(f"""select first_name, last_name, email, phone_number
+        cur.execute("""select first_name, last_name, email, phone_number
                         from clients c
                             join phones p on c.client_id = p.client_id
-                        where c.first_name = '{first_name}'
-                            or c.last_name = '{last_name}'
-                            or c.email = '{email}'
-                        ;""")
+                        where c.first_name = %s
+                            or c.last_name = %s
+                            or c.email = %s
+                        ;""", (first_name, last_name, email))
 
     for row in cur.fetchall():
         print(row)
 
 
-with psycopg2.connect(host = 'localhost', database="postgres", user="postgres", password="123") as conn:
-    create_db(conn)
+if __name__ == "__main__":
+    with psycopg2.connect(host = 'localhost', database="postgres", user="postgres", password="123") as conn:
+        change_client(conn,client_id=2, phones=777777)
 
-conn.close()
+    conn.close()
