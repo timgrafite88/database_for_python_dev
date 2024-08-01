12a2ddb78b9e6d0d63148b317d783e23c1c531a0
Timur Pustoplesnov <timur.pustoplesnov@yandex.ru>
2024-08-01 22:27:16 +0300

    update homework orm

--- ORM/main.py
+++ ORM/main.py
@@ -1,14 +1,37 @@
 import sqlalchemy
+from sqlalchemy import select
 from sqlalchemy.orm import sessionmaker
 from models import create_tables, Book, Shop, Publisher, Stock, Sale
 
-
+# подключение к базе PostgreSQL
 DSN = 'postgresql://postgres:123@localhost:5432/my_music_site'
+
 engine = sqlalchemy.create_engine(DSN)
 
-create_tables(engine)
+# #создание объектов
+# create_tables(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+publ = input('Введите Фамилию писателя: ')
+
+subq = session.query(Publisher).filter(Publisher.name == publ).all()
+
+stmnt = (
+    select(
+        #Book.title,Shop.name, Sale.price, Sale.date_sale
+        Sale
+        )
+#    .select(Shop)
+    .join(Stock, Sale.id_stock == Stock.id)
+    .join(Shop, Stock.id_shop == Shop.id)
+    .join(Book, Book.id == Stock.id_book)
+    .join(Publisher, Publisher.id == Book.id_publisher)
+    .where(Publisher.name == publ)
+)
+
+for i in session.scalars(stmnt):
+    print(i)
 
 session.close()
--- ORM/models.py
+++ ORM/models.py
@@ -9,12 +9,18 @@ class Publisher(Base):
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True)
 
+    def __str__(self):
+        return f'{self.name} |'
+
 
 class Shop(Base):
     __tablename__ = 'shop'
 
     id = sq.Column(sq.Integer, primary_key=True)
     name = sq.Column(sq.String(length=100), unique=True)
+
+    def __str__(self):
+        return f'{self.name} |'
 
 
 class Book(Base):
@@ -25,6 +31,9 @@ class Book(Base):
     id_publisher = sq.Column(sq.Integer, sq.ForeignKey('publisher.id'), nullable=False)
 
     publisher = relationship(Publisher, backref='book')
+
+    def __str__(self):
+        return f'{self.title} |'
 
 class Stock(Base):
     __tablename__ = 'stock'
@@ -37,6 +46,9 @@ class Stock(Base):
     book = relationship(Book, backref='stock')
     shop = relationship(Shop, backref='stock')
 
+    def __str__(self):
+        return f'{self.id_shop} | {self.count} |'
+
 class Sale(Base):
     __tablename__ = 'sale'
 
@@ -47,6 +59,9 @@ class Sale(Base):
     count = sq.Column(sq.Integer, nullable=False)
 
     stock = relationship(Stock, backref='sale')
+
+    def __str__(self):
+        return f'{self.price} | {self.date_sale} | {self.count} |'
 
 def create_tables(engine):
     Base.metadata.create_all(engine)
